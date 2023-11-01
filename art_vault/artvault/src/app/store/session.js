5be6b4dc8ccd3365e3e2f794f42ab22d607a74f2
Alex <acastro166@gmail.com>
2023-11-01 11:44:37 -0700

    formatting edits

--- art_vault/artvault/src/app/store/session.js
+++ art_vault/artvault/src/app/store/session.js
@@ -1,6 +1,22 @@
-// constants
+//-----Constants-----//
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
+
+
+//-----Reducer-----//
+const initialState = { user: null };
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_USER:
+      return { user: action.payload };
+
+    case REMOVE_USER:
+      return { user: null };
+
+    default: return state;
+  };
+};
+
 
 //-----Actions-----//
 const setUser = (user) => ({
@@ -78,20 +94,5 @@ export const logout = () => async (dispatch) => {
 
   if (response.ok) {
     dispatch(removeUser());
-  };
-};
-
-
-//-----Reducer-----//
-const initialState = { user: null };
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case SET_USER:
-      return { user: action.payload };
-
-    case REMOVE_USER:
-      return { user: null };
-
-    default: return state;
   };
 };
