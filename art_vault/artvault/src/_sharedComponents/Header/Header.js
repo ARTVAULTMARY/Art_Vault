5695d2f85497afd765073b8c9f785332d9ea55ac
Alex <acastro166@gmail.com>
2023-12-11 18:17:37 -0700

    Created custom color theme in tailwind using the artvault style guide

--- art_vault/artvault/src/_sharedComponents/Header/Header.js
+++ art_vault/artvault/src/_sharedComponents/Header/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="flex flex-col justify-center w-1366px h-360px">
-
+            <div className="flex flex-row justify-center w-full bg-lightGrey"></div>
         </div>
     )
 }
