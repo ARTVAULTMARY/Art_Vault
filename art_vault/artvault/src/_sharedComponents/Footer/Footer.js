44d0df8cf049b99c895176f8fbc3870a0a58e907
Alex <acastro166@gmail.com>
2023-12-18 15:38:44 -0700

    created footer component

---
+++ art_vault/artvault/src/_sharedComponents/Footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="flex flex-col items-center h-2/5 max-h-[300px] w-full min-w-[1200px] 2xl:w-5/6">
+
+        </div>
+    )
+}
--- art_vault/artvault/src/app/page.js
+++ art_vault/artvault/src/app/page.js
@@ -1,11 +1,11 @@
-import Link from 'next/link'
 import Header from '@/_sharedComponents/Header/Header'
+import Footer from '@/_sharedComponents/Footer/Footer'
 
 export default function Home() {
     return (
         <div className="flex flex-col w-full h-screen items-center bg-white">
             <Header />
-
+            <Footer />
         </div>
     )
 }
