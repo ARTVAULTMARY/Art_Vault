a093e6782d08edebaff3c0321396a7e5870a3f96
Alex <acastro166@gmail.com>
2024-01-02 11:34:00 -0700

    removed test text

--- art_vault/artvault/src/app/_Homepage/Homepage.js
+++ art_vault/artvault/src/app/_Homepage/Homepage.js
@@ -2,10 +2,10 @@ export default function Homepage() {
     return (
         <div className="flex flex-col items-center justify-center h-fit w-full min-w-[1200px] m-10 2xl:w-5/6 bg-white">
             <div>
-                Test
+            
             </div>
             <div>
-                Test
+            
             </div>
         </div>
     )
