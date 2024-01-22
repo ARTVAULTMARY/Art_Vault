4be408571ccaab44a612ac36004f07535dbe249e
Alex <acastro166@gmail.com>
2024-01-22 13:20:50 -0700

    added icons to HowItWorks

--- art_vault/artvault/src/app/_Homepage/HowItWorks.js
+++ art_vault/artvault/src/app/_Homepage/HowItWorks.js
@@ -5,35 +5,36 @@ export default function HowItWorks() {
 
             <div className="flex flex-row justify-evenly w-full h-[300px] bg-black">
 
-                <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
+                <div className="flex flex-row items-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">1</h2> 
                     <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
-                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">CREATE YOUR ACCOUNT</h2>
-                        <img className="h-1/2"></img>
+                        <h2 className="font-league-gothic h-1/2 text-superxl text-center">CREATE YOUR ACCOUNT</h2>
+                        <img src="/Images/paint_icon.svg" className="h-1/2 fill-teak"></img>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
+                <div className="flex flex-row items-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">2</h2> 
                     <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
-                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">SET UP YOUR GALLERY</h2>
-                        <img className="h-1/2"></img>
+                        <h2 className="font-league-gothic h-1/2 text-superxl text-center">SET UP YOUR GALLERY</h2>
+                        <img src="/Images/hanging_frame.svg" className="h-1/2 fill-teak"></img>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
+                <div className="flex flex-row items-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">3</h2> 
                     <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
-                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">SAVE YOUR ARTWORK</h2>
-                        <img className="h-1/2"></img>
+                        <h2 className="font-league-gothic h-1/2 text-superxl text-center">SAVE YOUR ARTWORK</h2>
+                        <img src="/Images/upload_icon.svg" className="h-1/2 fill-teak"></img>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
+                <div className="flex flex-row items-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">4</h2> 
-                    <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
-                        <h2 className="font-league-gothic h-1/2 text-4xl text-center"><span className="text-teak">SHARE YOUR ART </span>WITH THE WORLD</h2>
-                        <img className="h-1/2"></img>
+                    <div className="flex flex-col justify-center bg-white aspect-square p-6 rounded-md">
+                        <div className="flex font-league-gothic text-superxl text-center">
+                            <h2><span className="text-teak">SHARE YOUR ART</span> WITH THE WORLD</h2>
+                        </div>
                     </div>
                 </div>
 
--- art_vault/artvault/tailwind.config.js
+++ art_vault/artvault/tailwind.config.js
@@ -130,6 +130,9 @@ module.exports = {
                 'open-sans': 'var(--font-open-sans)',
                 'league-gothic': 'var(--font-league-gothic)',
             },
+            fontSize: {
+                'superxl': ['2.8rem', { lineHeight: '1' }],
+            },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
