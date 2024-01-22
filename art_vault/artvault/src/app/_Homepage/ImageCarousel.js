d6a41ccf9e847367a702c561d2a0bebceb69ca66
Alex <acastro166@gmail.com>
2024-01-22 13:39:31 -0700

    added image placeholders and styling to carousel

--- art_vault/artvault/src/app/_Homepage/ImageCarousel.js
+++ art_vault/artvault/src/app/_Homepage/ImageCarousel.js
@@ -1,8 +1,12 @@
 
 export default function ImageCarousel() {
     return (
-        <div className="flex flex-row justify-evenly w-full h-[600px]">
-            <div className="h-full w-full bg-carousel-large"></div>
+        <div className="flex flex-row w-full h-[600px]">
+            <div className="flex flex-row justify-evenly h-full w-full bg-carousel-large">
+                <img className="h-3/5 aspect-square mt-[80px] border-none bg-white shadow-2xl"></img>
+                <img className="h-3/5 aspect-square mt-[80px] border-none bg-white shadow-2xl"></img>
+                <img className="h-3/5 aspect-square mt-[80px] border-none bg-white shadow-2xl"></img>
+            </div>
         </div>
     )
 }
