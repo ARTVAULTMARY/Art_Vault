d173b355a586054a93cc2849e0329614a23f8d8c
Alex <acastro166@gmail.com>
2024-01-22 12:29:07 -0700

    fixed alignment issues

--- art_vault/artvault/src/app/_Homepage/HowItWorks.js
+++ art_vault/artvault/src/app/_Homepage/HowItWorks.js
@@ -7,33 +7,33 @@ export default function HowItWorks() {
 
                 <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">1</h2> 
-                    <div className="bg-white aspect-square p-8 rounded-md">
-                        <h2 className="font-league-gothic text-4xl text-center">CREATE YOUR ACCOUNT</h2>
-                        <img className=""></img>
+                    <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
+                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">CREATE YOUR ACCOUNT</h2>
+                        <img className="h-1/2"></img>
                     </div>
                 </div>
 
                 <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">2</h2> 
-                    <div className="bg-white aspect-square p-8 rounded-md">
-                        <h2 className="font-league-gothic text-4xl text-center">SET UP YOUR GALLERY</h2>
-                        <img className=""></img>
+                    <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
+                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">SET UP YOUR GALLERY</h2>
+                        <img className="h-1/2"></img>
                     </div>
                 </div>
 
                 <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">3</h2> 
-                    <div className="bg-white aspect-square p-8 rounded-md">
-                        <h2 className="font-league-gothic text-4xl text-center">SAVE YOUR ARTWORK</h2>
-                        <img className=""></img>
+                    <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
+                        <h2 className="font-league-gothic h-1/2 text-4xl text-center">SAVE YOUR ARTWORK</h2>
+                        <img className="h-1/2"></img>
                     </div>
                 </div>
 
                 <div className="flex flex-row items-center justify-center w-1/4 max-w-[270px] gap-5">
                     <h2 className="text-white text-5xl font-bold">4</h2> 
-                    <div className="bg-white aspect-square p-8 rounded-md">
-                        <h2 className="font-league-gothic text-4xl text-center">SHARE YOUR ART WITH THE WORLD</h2>
-                        <img className=""></img>
+                    <div className="flex flex-col bg-white aspect-square p-6 rounded-md">
+                        <h2 className="font-league-gothic h-1/2 text-4xl text-center"><span className="text-teak">SHARE YOUR ART </span>WITH THE WORLD</h2>
+                        <img className="h-1/2"></img>
                     </div>
                 </div>
 
