8eafb8e248ea43ee566cb82db5a82c1c5c3f773b
Alex <acastro166@gmail.com>
2024-02-27 03:00:12 -0700

    updated carousel background to most recent iteration

--- art_vault/artvault/src/app/_Homepage/ImageCarousel.js
+++ art_vault/artvault/src/app/_Homepage/ImageCarousel.js
@@ -2,7 +2,7 @@
 export default function ImageCarousel() {
     return (
         <div className="flex flex-row w-full h-[600px]">
-            <div className="flex flex-row justify-evenly h-full w-full bg-carousel-alternate bg-center">
+            <div className="flex flex-row justify-evenly h-full w-full bg-carousel bg-center">
                 <img className="h-3/5 aspect-square mt-[80px] border-none bg-shapes bg-center bg-cover shadow-carousel-image"></img>
                 <img className="h-3/5 aspect-square mt-[80px] border-none bg-spiral bg-center bg-cover shadow-carousel-image"></img>
                 <img className="h-3/5 aspect-square mt-[80px] border-none bg-hallway bg-center bg-cover shadow-carousel-image"></img>
