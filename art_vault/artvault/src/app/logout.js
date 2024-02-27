c4e08d99c7cfd4bfc73335a17fbbc3e7833f38a9
Alex <acastro166@gmail.com>
2024-02-27 15:12:17 -0700

    moved logout button to be rendered conditionally - replacing login  on the homepage if user session exists

--- art_vault/artvault/src/app/logout.js
+++ art_vault/artvault/src/app/logout.js
@@ -1,12 +1,13 @@
 'use client';
 import { signOut } from "next-auth/react";
+
 export default function Logout(){
     return(
-        <span 
+        <span className="flex ml-[145px] text-black font-league-gothic text-5xl tracking-wide hover:text-maroon-flush"
         onClick={() => {
-            signOut()
+            signOut();
         }}>
-            Logout
+            LOGOUT
         </span>
     )
 }
