5176234f3c63c326565a13327048462906b5b164
Alex <acastro166@gmail.com>
2024-02-29 11:16:26 -0700

    added logout button to conditionally render on user session

--- art_vault/artvault/src/_sharedComponents/Header/Header.js
+++ art_vault/artvault/src/_sharedComponents/Header/Header.js
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import { getServerSession } from 'next-auth'
+import Logout from '@/app/logout'
 
-export default function Header() {
+export default async function Header() {
+    const session = await getServerSession();
+
     return (
         <div className="flex flex-col items-center h-2/5 max-h-[300px] min-h-[300px] w-full min-w-[1200px] 2xl:w-5/6">
             <img
@@ -8,20 +12,20 @@ export default function Header() {
                 className="absolute h-1/4 max-h-[187px] min-h-[187px] scale-115 mt-2"
             />
             <div className="flex flex-row justify-start items-center w-full h-1/3 bg-alto">
-                <Link
+                { !session ? <Link
                     href='/register'
                     className="ml-36 text-black font-league-gothic text-5xl tracking-wide hover:text-maroon-flush"
                 >
                     SIGN UP
-                </Link>
+                </Link> : null } 
             </div>
             <div className="flex flex-row justify-between items-center w-full h-1/3 bg-white">
-                <Link
+                { !session ? <Link
                     href='/login'
                     className="flex ml-40 text-black font-league-gothic text-5xl tracking-wide hover:text-maroon-flush"
                 >
                     LOGIN
-                </Link>
+                </Link> : <Logout/> }
                 <div className="flex flex-row justify-center items-center w-1/3 h-full gap-10">
                     <button className="flex w-1/4 h-4/6 justify-center items-center tracking-wider bg-teak rounded-standard">
                         <Link
