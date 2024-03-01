d14df881360ca8cf21627db2f6ca4328072438ad
Alex <acastro166@gmail.com>
2024-03-01 01:17:24 -0700

    got basic login form working with auth

--- art_vault/artvault/src/app/test/components/Form/Login/LoginForm.jsx
+++ art_vault/artvault/src/app/test/components/Form/Login/LoginForm.jsx
@@ -1,39 +1,44 @@
 "use client"
 import useForm from "../useForm";
+import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { signIn } from 'next-auth/react'
 import { loginForm } from "../helpers/formConfig";
 
 export default function LoginForm() {
     const { renderFormInputs, isFormValid, form } = useForm(loginForm);
+    const [responseError, setResponseError] = useState("");
     const router = useRouter()
     console.log('theformishere', form)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('form data handleSubmit: ', form.email.value, form.password.value)
-        //TODO fix undefined data in response: 
         const response = await signIn('credentials', {
             username: form?.email.value,
             password: form?.password.value,
             redirect: false,
         });
 
-        if (!response?.error) {
+        if (response.ok) {
+            console.log({ response });
             router.push("/");
             router.refresh();
-        };
-        console.log({ response });
+        } else {
+            console.log(response.error);
+            setResponseError("Invalid Account Information")
+        }
     };
 
     return (
         <div className="flex flex-col w-full h-full">
             <form onSubmit={handleSubmit} className="flex flex-col w-full h-full py-10 justify-between items-center">
                 {renderFormInputs()}
+                <p className="h-5 text-sm text-maroon-flush m-0">{responseError}</p>
                 <button 
                     type="submit" 
                     disabled={!isFormValid()} 
-                    className="w-1/3 bg-teak-200 font-semibold mt-10 py-2 px-8 rounded enabled:bg-teak-300 enabled:cursor-pointer enabled:hover:bg-teak enabled:hover:text-white"
+                    formnovalidate="formnovalidate"
+                    className="w-1/3 bg-teak-200 font-semibold mt-2 py-2 px-8 rounded enabled:bg-teak-300 enabled:cursor-pointer enabled:hover:bg-teak enabled:hover:text-white"
                 >
                     Submit
                 </button>
