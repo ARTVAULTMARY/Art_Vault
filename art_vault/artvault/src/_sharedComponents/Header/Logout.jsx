d4ad4fd2ed7095719fe4a3e495d8b7b8c68a54cf
Alex <acastro166@gmail.com>
2024-03-19 14:19:15 -0700

    fixed logout button size and position

--- art_vault/artvault/src/_sharedComponents/Header/Logout.jsx
+++ art_vault/artvault/src/_sharedComponents/Header/Logout.jsx
@@ -3,11 +3,11 @@ import { signOut } from "next-auth/react";
 
 export default function Logout(){
     return(
-        <span className="flex ml-[145px] text-black font-league-gothic text-5xl tracking-wide hover:text-maroon-flush"
+        <span className="flex text-black font-league-gothic text-3xl justify-center hover:text-maroon-flush"
         onClick={() => {
             signOut({ callbackUrl: '/', redirect: true });
         }}>
-            LOGOUT
+            Logout
         </span>
     )
 }
