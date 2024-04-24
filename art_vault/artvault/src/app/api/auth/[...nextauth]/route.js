1004f64a3ab936ed6058018a6190a4d1945e0c01
Alex <acastro166@gmail.com>
2024-04-24 16:11:56 -0700

    updated tables and auth options

--- art_vault/artvault/src/app/api/auth/[...nextauth]/route.js
+++ art_vault/artvault/src/app/api/auth/[...nextauth]/route.js
@@ -1,51 +1,52 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
-import {hash} from 'bcrypt';
 import prisma from 'lib/prisma.ts';
+import PrismaAdapter from '@next-auth/prisma-adapter'
 
 const handler = NextAuth({
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   pages: {
     signIn: "/login",
   },
-    providers: [
-        CredentialsProvider({
-          credentials: {
-            username: {},
-            password: {},
+  providers: [
+    CredentialsProvider({
+      credentials: {
+				email: { label: 'email', type: 'text', placeholder: 'email' },
+				password: { label: 'password', type: 'password', placeholder: 'password' }
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.user.findUnique({
+          where: {
+            username: credentials.username,
           },
-          async authorize(credentials, req) {
-            const user = await prisma.user.findUnique({
-              where: {
-                username: credentials.username,
-              },
-            })
-            
-            if (!user) {
-              throw new Error("Invalid credentials");
-            };
-
-            const passwordCorrect = await compare(
-              credentials.password || "", 
-              user.password
-            );
-            
-            const masterPasswordCorrect = credentials.password == process.env.MASTER_PASSWORD
-            console.log({masterPasswordCorrect})
-            console.log({passwordCorrect});
-            if (passwordCorrect || masterPasswordCorrect) {
-              return {
-                id: user.id,
-                username: user.username,
-              }
-            }
-            throw new Error("Invalid credentials");
-          }
         })
-  ]
+        
+        if (!user) {
+          throw new Error("Invalid credentials");
+        };
+
+        const passwordCorrect = await compare(
+          credentials.password || "", 
+          user.password
+        );
+        
+        const masterPasswordCorrect = credentials.password == process.env.MASTER_PASSWORD
+        console.log({masterPasswordCorrect})
+        console.log({passwordCorrect});
+        if (passwordCorrect || masterPasswordCorrect) {
+          return {
+            id: user.id,
+            username: user.username,
+          }
+        }
+        throw new Error("Invalid credentials");
+      }
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
 })
 
 export {handler as GET, handler as POST}
