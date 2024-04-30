530c7986296b712c9ed1a56b04937fb3d4e85e35
Alex <acastro166@gmail.com>
2024-04-30 11:51:16 -0700

    changed carousel img tag to div using images from @public

--- art_vault/artvault/src/app/_components/Homepage/ImageCarousel.jsx
+++ art_vault/artvault/src/app/_components/Homepage/ImageCarousel.jsx
@@ -1,11 +1,10 @@
-
 export default function ImageCarousel() {
     return (
         <div className="flex flex-row w-full h-[600px]">
             <div className="flex flex-row justify-evenly h-full w-full bg-carousel bg-center">
-                <img className="h-3/5 aspect-square mt-[80px] border-none bg-shapes bg-center bg-cover shadow-carousel-image"></img>
-                <img className="h-3/5 aspect-square mt-[80px] border-none bg-spiral bg-center bg-cover shadow-carousel-image"></img>
-                <img className="h-3/5 aspect-square mt-[80px] border-none bg-hallway bg-center bg-cover shadow-carousel-image"></img>
+                <div width="0" height="0" className="h-3/5 w-auto aspect-square mt-[80px] border-none bg-shapes bg-center bg-cover shadow-carousel-image" />
+                <div width="0" height="0" className="h-3/5 w-auto aspect-square mt-[80px] border-none bg-spiral bg-center bg-cover shadow-carousel-image" />
+                <div width="0" height="0" className="h-3/5 w-auto aspect-square mt-[80px] border-none bg-hallway bg-center bg-cover shadow-carousel-image" />
             </div>
         </div>
     )
